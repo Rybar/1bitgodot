5a2a76564330ba43ed6ecbfd8e6f5ad569e77a58
Ryan Malm <ryan.malm@us.af.mil>
2020-06-12 23:15:30 -0500

    tilemap setup

--- 1BitCourse Metroidvania/Player.gd
+++ 1BitCourse Metroidvania/Player.gd
@@ -1,23 +1,26 @@
 extends KinematicBody2D
 
 export (int) var ACCELERATION = 512
-export (int) var MAX_SPEED = 64
+export (int) var MAX_SPEED = 100
 export (float) var FRICTION = 0.25
-export (int) var GRAVITY = 200
-export (int) var JUMP_FORCE = 128
+export (int) var GRAVITY = 250
+export (int) var JUMP_FORCE = 180
 export (int) var MAX_SLOPE_ANGLE = 46
 
 
 var motion = Vector2.ZERO
+var snap_vector = Vector2.ZERO
+var just_jumped = false
 
 onready var sprite = $Sprite
 onready var spriteAnimator = $SpriteAnimator
 
 func _physics_process(delta):
-	
+	just_jumped = false
 	var input_vector = get_input_vector()
 	apply_horizontal_force(input_vector, delta)
 	apply_friction(input_vector)
+	update_snap_vector()
 	jump_check()
 	apply_gravity(delta)
 	update_animations(input_vector)
@@ -37,10 +40,16 @@ func apply_friction(input_vector):
 	if input_vector.x == 0:
 		motion.x = lerp(motion.x, 0, FRICTION)
 		
+func update_snap_vector():
+	if is_on_floor():
+		snap_vector = Vector2.DOWN
+		
 func jump_check():
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
+			just_jumped = true
 			motion.y = -JUMP_FORCE
+			snap_vector = Vector2.ZERO
 	else:
 		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
 			motion.y = -JUMP_FORCE/2;
@@ -48,6 +57,7 @@ func jump_check():
 func apply_gravity(delta):
 		motion.y += GRAVITY * delta
 		motion.y = min(motion.y, JUMP_FORCE)
+
 		
 func update_animations(input_vector):
 	if input_vector.x != 0:
@@ -60,4 +70,25 @@ func update_animations(input_vector):
 		spriteAnimator.play("Jump")
 			
 func move():
-	motion = move_and_slide(motion, Vector2.UP)
+	var was_in_air = not is_on_floor()
+	var was_on_floor = is_on_floor()
+	var last_motion = motion
+	var last_position = position
+	
+	motion = move_and_slide_with_snap(motion, snap_vector * 4, Vector2.UP, true, 4, deg2rad(MAX_SLOPE_ANGLE))
+	
+	#landing
+	if was_in_air and is_on_floor():
+		motion.x = last_motion.x
+		
+	#just left ground
+	if was_on_floor and not is_on_floor() and not just_jumped:
+		motion.y = 0
+		position.y = last_position.y
+		
+	#prevent sliding hack
+	if is_on_floor() and last_position.y != position.y and get_floor_velocity().length() == 0 and abs(motion.x) < 5:
+		position.x = last_position.x
+		
+	
+	
